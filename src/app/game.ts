f6630f315008748443623cd32efc8c1f776a099e
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-12-17 22:31:44 -0600

    add games component and model

---
+++ src/app/game.ts
@@ -0,0 +1,11 @@
+export class Game {
+    _id: string;
+    gameName: string;
+    description: string;
+    released: string;
+    platform: string;
+    developer: string;
+    publisher: string;
+    genre: string;
+    esrb_rating: string;
+}
